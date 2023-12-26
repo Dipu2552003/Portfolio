d9cda256032eb2dc609f7f67e3970f518a46d142
Dipkul <deepkul2552003@gmail.com>
2023-12-26 22:12:43 +0530

    done

---
+++ src/content_option.js
@@ -0,0 +1,186 @@
+const logotext = "DK";
+const meta = {
+    title: "Dipkul Khandelwal",
+    description: "I’m Dipkul Khandelwal , currently pursing Computer Engineering from VIT",
+};
+
+const introdata = {
+    title: "I’m Dipkul Khandelwal",
+    animated: {
+        first: "Designer",
+        second: "Coder",
+        third: "I develop cool websites",
+    },
+    description: "Greetings! I'm Dipkul Khandelwal, a Computer Engineering student at VIT. Currently, I'm immersed in web development and the world of design, constantly learning and expanding my horizons. Open to collaborations – feel free to get in touch!. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Hey there! I'm Dipkul, a student and aspiring developer currently exploring the exciting world of B Tech in Computer Science and Engineering at Vishwakarma Institute of Technology, Pune. I'm all about crafting cool software and diving into projects that keep the adrenaline pumping. Currently on the lookout for internship opportunities as a web and software developer – because, why not turn passion into a profession, right? Originally from the vibrant city of Pune, my journey started there.Feel free to roam around my digital space, and if you're into tech, coding, or just want to share a good laugh, you're in the right place. Let's connect and make the web a little more awesome! 🚀😊"
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "C",
+        value: 65,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "SQL",
+        value: 70,
+    },
+   
+];
+
+const timeline = [{
+        title: "2023",
+        description: "Design Head in TedxVITPune , Started my Web Developer Journey",         
+    },
+    {
+        title: "2022",
+        description: " Cordinator at SocialWelfare Club ,Started Investing 📈 Used my earnings and invested a small part into Indian Stock Market.",
+    },
+    {
+        title: "2021",
+        description: "Completed 12th grade and aced the competitive exams, including JEE Mains and Advance.Degree College 🏫 Got admitted to Vishwakarma Institute of Technology., Taught coding 👨‍🏫",
+    },
+    {
+        title: "2020",
+        description: "Started Understanding laws of universe with physic ",
+    },
+    {
+        title: "2019",
+        description: "Completed 10th",
+    },
+    {
+        title: "2018",
+        description: "President of Computer Scoiety in School , Won best Animator Award",
+    },
+    {
+        title: "2011",
+        description: "First Computer at home 💾, I remember playing roadrash every night chance by chance with my brother!",
+    },
+    {
+        title: "2005",
+        description: "Went to school 🏫",
+    },
+    {
+        title: "2003",
+        description: "Born 👶🏼🍼",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "deepkul2552003@gmail.com",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Dipu2552003",
+    linkedin: "https://www.linkedin.com/in/dipkul-khandelwal-8628a3157/",
+    twitter: "",
+    behance: "https://www.behance.net/dipkulkhandel"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    timeline,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
